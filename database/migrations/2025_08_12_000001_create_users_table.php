56b2dcdac4608c99fe7939db6d8c5d0c5df76332
Sujon <sujonbdjoin019@gmail.com>
2025-08-12 15:02:44 +0600

    model and migration file creation and table schema

--- database/migrations/2025_08_12_000001_create_users_table.php
+++ database/migrations/2025_08_12_000001_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('role_name')->default('Customer')->comment('Admin and Customer Role Define Fix and default role_name: Customer.');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
---
+++ database/migrations/2025_08_12_084541_create_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('service_id')->constrained('services')->cascadeOnDelete();
+            $table->date('booking_date');
+            $table->string('status', [1,2,3])->default(1)->comment('1 = Pending, 2 = Confirmed, 3 = Cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
