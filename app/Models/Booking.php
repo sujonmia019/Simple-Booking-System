9124a974403b984a786ab47321b3f8a747a73696
Sujon <sujonbdjoin019@gmail.com>
2025-08-12 15:11:29 +0600

    model ORM relation method define

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'bookings';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = ['user_id','service_id','booking_date','status'];
+
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function service(){
+        return $this->belongsTo(Service::class, 'service_id');
+    }
 }
--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'services';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = ['name','description','price','status'];
+
+    public function bookings(){
+        return $this->hasMany(Booking::class, 'service_id');
+    }
 }
