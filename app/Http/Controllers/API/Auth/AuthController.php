49a8cbebe31a91c2530709a29bd2efd06a021a39
Sujon <sujonbdjoin019@gmail.com>
2025-08-18 11:06:24 +0600

    pull request for work from home

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken; // token generate
 
-         return $this->responseJson(
+        return $this->responseJson(
             'success',
             'Registration successful',
             [
